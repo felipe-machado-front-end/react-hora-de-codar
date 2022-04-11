02862d1b708030fb4298c10f7944a26035a0d3eb
Felipe Machado <felipe.machado.front.end@gmail.com>
2022-04-11 16:34:34 -0300

    Componentes devem ser envolvidor por uma tag

---
+++ src/components/1-fundamentos/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FirstComponent = () => {
+  return (
+    <div>
+      <p>Todos componentes devem ser envolvidor por uma tag</p>
+    </div>
+  );
+};
+
+export default FirstComponent;
