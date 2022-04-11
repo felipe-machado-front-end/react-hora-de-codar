164108e28f304867ef8b6df329dcb4832579668d
Felipe Machado <felipe.machado.front.end@gmail.com>
2022-04-11 20:31:30 -0300

    Importando uma imagem da pasta assets e usando no componente Photo

--- src/components/2-avancando-react/UseComponents2.jsx
+++ src/components/2-avancando-react/UseComponents2.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Photo from "./Photo";
 
 export const UseComponents2 = () => {
   return (
     <div>
       <h2>Componentes da seção 03 </h2>
+      <Photo />
     </div>
   );
 };
