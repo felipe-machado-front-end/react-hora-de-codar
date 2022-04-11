696f3dd6cbed0f7ca8baf64c504e94ba6ddcbdfc
Felipe Machado <felipe.machado.front.end@gmail.com>
2022-04-11 16:42:29 -0300

    Expressões JSX podem ser usados para imprimir um valor de um objeto ou até fazer uma operação mátematica,para invocar essa expressão basta usar as {}

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import TemplateExpression from "./components/1-fundamentos/TemplateExpression";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <TemplateExpression />
   </React.StrictMode>,
   document.getElementById("root")
 );
