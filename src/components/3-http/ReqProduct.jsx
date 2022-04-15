47d8955ce4b09f037b966b5089062e418b753f0b
Felipe <60829850+felipe-machado-front-end@users.noreply.github.com>
2022-04-15 10:32:37 -0300

    Criando requisição post

--- src/components/3-http/ReqProduct.jsx
+++ src/components/3-http/ReqProduct.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import "../../App.css";
 
 const ReqProduct = () => {
   const url = "http://localhost:3000/products";
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(url);
@@ -11,6 +14,21 @@ const ReqProduct = () => {
     }
     fetchData();
   }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  };
 
   return (
     <div>
@@ -22,6 +40,29 @@ const ReqProduct = () => {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            NOME
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            PREÇO
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 };
