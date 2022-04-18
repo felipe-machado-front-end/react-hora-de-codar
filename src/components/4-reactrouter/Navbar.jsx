de98296a2c553c17459b61e2068874d9bdaa6d3d
Felipe <60829850+felipe-machado-front-end@users.noreply.github.com>
2022-04-18 17:11:42 -0300

    usando navLink
    Util para ficar marcado o item do menu selecionado

--- src/components/4-reactrouter/Navbar.jsx
+++ src/components/4-reactrouter/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink
+        to={"/"}
+        className={({ isActive }) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink to={"/about"}>Sobre</NavLink>
     </nav>
   );
 };
