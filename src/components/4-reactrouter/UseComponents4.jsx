081b9c6d59e70ddb07ba3fee31aa5d40a1aa8d9b
Felipe <60829850+felipe-machado-front-end@users.noreply.github.com>
2022-04-18 15:24:57 -0300

    Configurando react router
    Criando uma navegação simples ( necessário digitar url)

--- src/components/4-reactrouter/UseComponents4.jsx
+++ src/components/4-reactrouter/UseComponents4.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "../../pages/About";
+import Home from "../../pages/Home";
 
 const UseComponents4 = () => {
-  return <h1>Usando React Router</h1>;
+  return (
+    <div>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default UseComponents4;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Pagina inicial</div>;
+};
+
+export default Home;
