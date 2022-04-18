25b7be03335023c296841ab13ce1457dd6f1e6db
Felipe <60829850+felipe-machado-front-end@users.noreply.github.com>
2022-04-18 15:34:08 -0300

    Usando link do react router

--- src/components/4-reactrouter/UseComponents4.jsx
+++ src/components/4-reactrouter/UseComponents4.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../../pages/About";
 import Home from "../../pages/Home";
+import Navbar from "./Navbar";
 
 const UseComponents4 = () => {
   return (
     <div>
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
