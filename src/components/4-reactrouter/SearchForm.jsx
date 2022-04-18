60c53fee0678542c2e52c3bde915480b438c65b5
Felipe <60829850+felipe-machado-front-end@users.noreply.github.com>
2022-04-18 17:46:44 -0300

    Fazendo a busca atráves de um form

---
+++ src/components/4-reactrouter/SearchForm.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const SearchForm = () => {
+  const navigate = useNavigate();
+  const [query, setQuery] = React.useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate("/search?q=" + query);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <input type="submit" value="Buscar" />
+    </form>
+  );
+};
+
+export default SearchForm;
--- src/components/4-reactrouter/UseComponents4.jsx
+++ src/components/4-reactrouter/UseComponents4.jsx
@@ -5,7 +5,9 @@ import Home from "../../pages/Home";
 import Info from "../../pages/Info";
 import NotFound from "../../pages/NotFound";
 import Product from "../../pages/Product";
+import Search from "../../pages/Search";
 import Navbar from "./Navbar";
+import SearchForm from "./SearchForm";
 
 const UseComponents4 = () => {
   return (
@@ -13,11 +15,13 @@ const UseComponents4 = () => {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/products/:id" element={<Product />}></Route>
           <Route path="/products/:id/info" element={<Info />}></Route>
+          <Route path="/search" element={<Search />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
